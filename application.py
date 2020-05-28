6b133ec16e260cb3a97f5864c5c7b227337068d8
greggbernelegarda <gregglegarda@gmail.com>
2020-05-27 21:25:17 -0400

    Video stream

--- application.py
+++ application.py
@@ -1,73 +1,72 @@
-from flask import Flask
+
+from flask import Flask,request,jsonify
+import numpy as np
+import cv2
+import tensorflow as tf
+import base64
+
+
+
+
 app = Flask(__name__)
-
-@app.route("/")
-def main():
-    main = layout()
-    return main
-
-#==============================================   LAYOUT  =================================================#
-def layout():
-    layout = header() + body() + footer()
-    return layout
-
-def header():
-    from scripts.layout import header
-    header1 = header.header()
-    header = header1.header_samples()
-    return header  + "</br>"
-
-def body():
-    nc = container_main()
-    from scripts.layout import body
-    body1 = body.body()
-    body = body1.body_samples()
-    return nc + body + "</br>"
-
-def footer():
-    from scripts.layout import footer
-    footer1 = footer.footer()
-    footer = footer1.footer_samples()
-    return footer + "</br>"
-
-#==============================================   CONTAINERS  =================================================#
-def container_main():
-    main_container =  news_container() + radio_container() + music_container()
-    return main_container
-
-def news_container():
-    from scripts.channels.news import abscbn
-    from scripts.channels.news import gma
-    v1 = abscbn.video()
-    video1 = v1.video_samples()
-    v2 = gma.video()
-    video2 = v2.video_samples()
-    video = video1 + " " + video2
-    return video + "</br>"
+graph = tf.get_default_graph()
 
 
-def radio_container():
-    from scripts.channels.radio import opmlovesongs
-    from scripts.channels.radio import teleradyo
+@app.route('/')
+def hello_world():
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+    <video id="video" width="640" height="480" autoplay></video>
+    <button id="snap">Snap Photo</button>
+    <canvas id="canvas" width="640" height="480"></canvas>
+    </body>
+    <script>
 
-    v1 = opmlovesongs.video()
-    video1 = v1.video_samples()
+    var video = document.getElementById('video');
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+            //video.src = window.URL.createObjectURL(stream);
+            video.srcObject = stream;
+            video.play();
+        });
+    }
 
-    v2 = teleradyo.video()
-    video2 = v2.video_samples()
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
 
-    ###concatenate as a string
-    video = video1 +" " + video2
-    return video + "</br>"
-
-def music_container():
-    from scripts.channels.music import monstercat
-    v1 = monstercat.video()
-    video1 = v1.video_samples()
-    ###concatenate as a string
-    video = video1
-    return video + "</br>"
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+    var request = new XMLHttpRequest();
+    request.open('POST', '/submit?image=' + video.toString('base64'), true);
+    request.send();
+    });
 
 
+
+</script>
+</html>
+    """
+
+# HtmlVideoElement
+
+@app.route('/test',methods=['GET'])
+def test():
+    return "hello world!"
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    image = request.args.get('image')
+
+
+    print(type(image))
+    return ""
 
 
